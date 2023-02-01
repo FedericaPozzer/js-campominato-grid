989def754122ef322bc9004ccf05f38686913293
Federica Pozzer <federica.pozzer@gmail.com>
2023-02-01 16:16:56 +0100

    not done (TODO numbers and bonus)

--- js/script.js
+++ js/script.js
@@ -5,14 +5,12 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 
-// Richiamo il bottone già presente nell'HTML
-// Addeventlistener con il click sul bottone
-    // Genero griglia con una funzione
-        // FUNZIONE GRIGLIA
-    // BOH
+/************************************
+ *              ON LOAD             *
+ ***********************************/
 
 
-// richiamo il bottone 
+// richiamo il bottone dall'HTML
 let my_button = document.getElementById("start-button");
 // addeventlistener con il click sul bottone
 my_button.addEventListener(
@@ -24,46 +22,47 @@ my_button.addEventListener(
         const grid_el = document.getElementById("grid");
         // collego la funzione griglia con la girglia pescata dall'HTML
         generate_grid(grid_el);
-
-
-        /********************************
-         *          FUNZIONI            *
-         *******************************/
-
-
-        // funzione per creare la griglia con dentro le celle numerate
-        function generate_grid(grid) {
-
-
-            // le celle devono essere numerate, quindi:
-                // dichiaro array lista numeri (vuota)
-            let lista_numeri = [];
-                // condizioni per generare i numeri
-            for (let i = 0; i < 100; i++) {
-                    // pusho i numeri generati nell'array
-                lista_numeri.push(i + 1);
-            }
-            // funziona? NOPE -- perchè? AAAAH ok, perchè siamo dentro a un click iniziale -.-" ok, funziona!
-            console.log(lista_numeri);
-
-            // now, ogni volta che genro una cella la devo numerare :)
-            // COME? BOH!
-
-            
-
-            for (let i = 0; i < 100; i++) {
-                // creo le celle
-                const cell_el = document.createElement("div");
-                cell_el.classList.add("square");
-                grid.append(cell_el);
-
-                // "quando clicco sulla cella, cambia colore"
-                cell_el.addEventListener("click",
-                    function () {
-                        this.classList.toggle("clicked");
-                    }
-                )
-            }
-        };
     }
 )
+
+
+/************************************
+ *            FUNCTIONS             *
+ ***********************************/
+
+
+// funzione per creare la griglia con dentro le celle numerate
+function generate_grid(grid) {
+    
+    // svuoto la grid (nel caso sia piena, così se clicco "start game" più volte non comapiono griglie sotto)
+    grid.innerHTML = "";
+
+    // le celle devono essere numerate, quindi:
+    // dichiaro array lista numeri (vuota)
+    let lista_numeri = [];
+    // condizioni per generare i numeri
+    for (let i = 0; i < 100; i++) {
+        // pusho i numeri generati nell'array
+        lista_numeri.push(i + 1);
+    }
+    // funziona? NOPE -- perchè? AAAAH ok, perchè siamo dentro a un click iniziale -.-" ok, funziona!
+    console.log(lista_numeri);
+
+    // now, ogni volta che genero una cella la devo numerare :)
+    // COME? BOH! Cioè so che devo append il numero nell'HTML but how???
+
+
+    for (let i = 0; i < 100; i++) {
+        // creo le celle
+        const cell_el = document.createElement("div");
+        cell_el.classList.add("square");
+        grid.append(cell_el);
+
+        // "quando clicco sulla cella, cambia colore"
+        cell_el.addEventListener("click",
+            function () {
+                this.classList.toggle("clicked");
+            }
+        )
+    }
+};
